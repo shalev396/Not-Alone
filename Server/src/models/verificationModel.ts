ebe6149b4b51f54e08d135ac819481b4b16a3201
shalev396 <shalev396@gmail.com>
2025-01-31 12:10:49 +0200

    Comment out email verification code and related model definitions

--- Server/src/models/verificationModel.ts
+++ Server/src/models/verificationModel.ts
@@ -1,19 +1,19 @@
-import mongoose from "mongoose";
-type Verification = {
-  email: string;
-  verificationCode: string;
-  expiresAt: Date;
-};
+// import mongoose from "mongoose";
+// type Verification = {
+//   email: string;
+//   verificationCode: string;
+//   expiresAt: Date;
+// };
 
-const verificationSchema = new mongoose.Schema<Verification>({
-  email: { type: String, required: true },
-  verificationCode: { type: String, required: true },
-  expiresAt: { type: Date, required: true },
-});
+// const verificationSchema = new mongoose.Schema<Verification>({
+//   email: { type: String, required: true },
+//   verificationCode: { type: String, required: true },
+//   expiresAt: { type: Date, required: true },
+// });
 
-const verificationModel = mongoose.model<Verification>(
-  "Verification",
-  verificationSchema
-);
+// const verificationModel = mongoose.model<Verification>(
+//   "Verification",
+//   verificationSchema
+// );
 
-export default verificationModel;
+// export default verificationModel;
